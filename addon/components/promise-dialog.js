48ff2edaa3191ce5b8a2db67485eceed29a33123
Ryan Tablada <ryan.tablada@gmail.com>
2016-02-20 09:48:23 -0600

    Check that `isActive` is shown

--- addon/components/promise-dialog.js
+++ addon/components/promise-dialog.js
@@ -1,8 +1,32 @@
 import Ember from 'ember';
 import layout from '../templates/components/promise-dialog';
 
+const { computed, get } = Ember;
+
 export default Ember.Component.extend({
   layout,
+
+  classNameBindings: ['active'],
+  dialogType: null,
+
+  activeClass: 'active',
+
+  isActive: computed('promise', 'options.type', 'dialogType', function () {
+    let { promise, options, dialogType } = this.getProperties('promise', 'options', 'dialogType');
+    options = options || {};
+
+    if (get(this, 'promise')) {
+      return dialogType && options.type ? dialogType === options.type : true;
+    }
+
+    return false;
+  }),
+
+  active: computed('isActive', 'activeClass', function () {
+    return get(this, 'isActive') ?
+      get(this, 'activeClass') :
+      false;
+  }),
 
   service: Ember.inject.service('dialog'),
 
--- tests/integration/components/promise-dialog-test.js
+++ tests/integration/components/promise-dialog-test.js
@@ -1,24 +1,40 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('promise-dialog', 'Integration | Component | promise dialog', {
-  integration: true
+const dialogStub = Ember.Service.extend({
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
+moduleForComponent('promise-dialog', 'Integration | Component | promise dialog', {
+  integration: true,
 
-  this.render(hbs`{{promise-dialog}}`);
+  beforeEach() {
+    this.register('service:dialog', dialogStub);
+    this.inject.service('dialog', { as: 'dialog' });
+  },
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it renders basic active state', function (assert) {
+  Ember.run(() => {
+    // Template block usage:"
+    this.render(hbs`
+      {{#promise-dialog}}
+      This should not render
+      {{/promise-dialog}}
+    `);
 
-  // Template block usage:"
-  this.render(hbs`
-    {{#promise-dialog}}
-      template block text
-    {{/promise-dialog}}
-  `);
+    assert.equal(this.$().text().trim(), '');
+    assert.equal(this.$('.active').length, 0);
 
-  assert.equal(this.$().text().trim(), '');
+    this.dialog.set('promise', true);
+
+    this.render(hbs`
+      {{#promise-dialog wat=true}}
+      This is rendered
+      {{/promise-dialog}}
+    `);
+
+    assert.equal(this.$().text().trim(), 'This is rendered');
+    assert.equal(this.$('.active').length, 1);
+  });
 });
